a054f8e79f87807321977c44326ed03646d17282
Ibrahim Amjad <heemaplus242@gmail.com>
2025-08-07 01:46:10 +0200

    Update ALLOWED_HOSTS for Render domain

--- Casablanca/settings.py
+++ Casablanca/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -130,5 +131,4 @@ MEDIA_ROOT = 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = '/login/'
-ALLOWED_HOSTS = ['*']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+ALLOWED_HOSTS = ['casablanca-egx2.onrender.com']
